385fbea54643ae8fe00a5e9fe36361df92716e28
BeFreeI <nepogodin_p@mail.ru>
2020-11-03 15:04:56 +0300

    update

--- server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const { PORT } = require('./src/common/config');
-
-app.listen(PORT, () => {
-  console.log(`Server is running in  http://localhost:${PORT}\\`);
-});
--- src/common/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  PORT: 8000,
-  PrinterApp: process.cwd() + "\\NiceLableSDK\\ConsoleApp1\\bin\\Debug\\ConsoleApp1.exe"
-}
